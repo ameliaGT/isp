e8e672ee397fe3221a76fc7c8c24de1d914c0867
Amelia <ameliagarciatorres@hotmail.com>
2021-08-03 11:38:23 +0200

    primer commit

---
+++ Dog.java
@@ -0,0 +1,12 @@
+public class Dog implements Animal {
+
+    public void run() {
+        System.out.print("Dog is running");
+
+    }
+
+    public void bark() {
+        System.out.print("Dog is barking");
+
+    }
+}
